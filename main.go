106748f4f068bd56ad59125c3a7394552953f8fb
Mihail Buravcov <mburavczov@gmail.com>
2023-12-05 16:48:48 +0300

    fix

--- main.go
+++ main.go
@@ -2,59 +2,111 @@ package main
 
 import (
 	"database/sql"
+	"errors"
 	"fmt"
 
 	_ "modernc.org/sqlite"
 )
 
+type Client struct {
+	ID       int
+	FIO      string
+	Login    string
+	Birthday string
+	Email    string
+}
+
+func (c Client) String() string {
+	return fmt.Sprintf("ID: %d FIO: %s Login: %s Birthday: %s Email: %s",
+		c.ID, c.FIO, c.Login, c.Birthday, c.Email)
+}
+
 func main() {
 	db, err := sql.Open("sqlite", "demo.db")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 	defer db.Close()
 
-	fio := // укажите ФИО
-	login := // укажите логин
-	birthday := // укажите день рождения
-	email := // укажите почту
-
-	id := insertClient(db, fio, login, birthday, email)
-	selectClient(db, id)
-
-	newLogin := // укажите новый логин
-	updateClientLogin(db, newLogin, id)
-	selectClient(db, id)
-
-	deleteClient(db, id)
-	selectClient(db, id)
-}
-
-func insertClient(db *sql.DB, fio, login, birthday, email string) int64 {
-	// напишите здесь код для добавления новой записи в таблицу clients
-
-	return 0 // вместо 0 верните идентификатор добавленной записи
-}
-
-func updateClientLogin(db *sql.DB, login string, id int64) {
-	// напишите здесь код для обновления поля login в таблице clients у записи с заданным id
-}
-
-func deleteClient(db *sql.DB, id int64) {
-	// напишите здесь код для удаления записи из таблицы clients по заданному id
-}
-
-func selectClient(db *sql.DB, clientId int64) {
-	var id int64
-	var fio string
-	var login string
-	var birthday string
-	var email string
-
-	err := db.QueryRow("SELECT id, fio, login, birthday, email FROM clients WHERE id = ?", clientId).Scan(&id, &fio, &login, &birthday, &email)
-	if err != nil {
-		panic(err)
+	// добавление нового клиента
+	newClient := Client{
+		FIO:      "", // укажите ФИО
+		Login:    "", // укажите логин
+		Birthday: "", // укажите день рождения
+		Email:    "", // укажите почту
 	}
 
-	fmt.Println(id, fio, login, birthday, email)
+	id, err := insertClient(db, newClient)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// получение клиента по идентификатору и вывод на консоль
+	client, err := selectClient(db, id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(client)
+
+	// обновление логина клиента
+	newLogin := "" // укажите новый логин
+	err = updateClientLogin(db, newLogin, id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// получение клиента по идентификатору и вывод на консоль
+	client, err = selectClient(db, id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(client)
+
+	// удаление клиента
+	err = deleteClient(db, id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// получение клиента по идентификатору и вывод на консоль
+	_, err = selectClient(db, id)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			fmt.Printf("Клиент с идентификатором %d не найден", id)
+		} else {
+			fmt.Println(err)
+		}
+		return
+	}
+}
+
+func insertClient(db *sql.DB, client Client) (int64, error) {
+	// напишите здесь код для добавления новой записи в таблицу clients
+
+	return 0, nil // вместо 0 верните идентификатор добавленной записи
+}
+
+func updateClientLogin(db *sql.DB, login string, id int64) error {
+	// напишите здесь код для обновления поля login в таблице clients у записи с заданным id
+	return nil
+}
+
+func deleteClient(db *sql.DB, id int64) error {
+	// напишите здесь код для удаления записи из таблицы clients по заданному id
+	return nil
+}
+
+func selectClient(db *sql.DB, clientId int64) (Client, error) {
+	client := Client{}
+
+	row := db.QueryRow("SELECT id, fio, login, birthday, email FROM clients WHERE id = ?", clientId)
+	err := row.Scan(&client.ID, &client.FIO, &client.Login, &client.Birthday, &client.Email)
+
+	return client, err
 }
